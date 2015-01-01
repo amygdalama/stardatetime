b9882d7535583cdeccbfbaa3e34bbf86cc808d1b
Amy Hanlon <amyehanlon@gmail.com>
2015-01-01 12:07:24 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stardate',
+      version='0.1',
+      description='datetime for the next generation',
+      url='https://github.com/amygdalama/stardate',
+      author='Amy Hanlon',
+      license='MIT',
+      packages=['stardate'],
+      zip_safe=False)
+
