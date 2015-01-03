716e1478211f9bf19a0fa449d51821ec83ee0c25
Amy Hanlon <amyehanlon@gmail.com>
2015-01-03 11:13:11 -0500

    remove trailing whitespace

--- stardatetime/conversion.py
+++ stardatetime/conversion.py
@@ -15,6 +15,6 @@ def earth_date_to_star_date(earth_year, earth_month, earth_day):
     star_year = (earth_year - 2323) * 1000
     first_date_of_year = date(year=earth_year, month=1, day=1)
     earth_date = date(year=earth_year, month=earth_month, day=earth_day)
-    days_elapsed_in_year = earth_date - first_date_of_year 
+    days_elapsed_in_year = earth_date - first_date_of_year
     star_day = round(days_elapsed_in_year.days / 365.0 * 1000, 1)
     return star_year + star_day
