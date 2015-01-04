6ea7d3388d4fc6b7854c6841c51580e55df32024
Amy Hanlon <amyehanlon@gmail.com>
2015-01-03 19:02:02 -0500

    import classes inside __init__

--- stardatetime/__init__.py
+++ stardatetime/__init__.py
@@ -5,3 +5,11 @@ Modules:
     conversion.py -- helper module for converting Earth
         time to Star Trek time
 """
+from __future__ import absolute_import
+from stardatetime.stardatetime import (StarDate, StarDateTime,
+                                       StarTime, StarTimeDelta)
+
+assert StarDate
+assert StarDateTime
+assert StarTime
+assert StarTimeDelta
