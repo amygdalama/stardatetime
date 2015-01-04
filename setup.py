0dd05d45e67b661a92cfdfe5c88ee0b3f341318d
Amy Hanlon <amyehanlon@gmail.com>
2015-01-03 21:33:02 -0500

    increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='stardatetime',
-      version='0.1',
+      version='0.1.1',
       description='datetime for the next generation',
       url='https://github.com/amygdalama/stardatetime',
       author='Amy Hanlon',
